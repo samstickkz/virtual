422dba6962a6d3e00bc69ea177fcb1cf2ad86aa6
samstickkz <samstickkz@gmail.com>
2024-07-11 16:33:55 +0100

    up nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo.png";
+import { navItems } from "../constants";
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-Lg border-b border-neutral-700/800">
@@ -8,7 +9,24 @@ const Navbar = () => {
             <img className="h-10 w-10 mr-2" src={logo} alt="" />
             <span className="text-xl tracking-tight">Virtual Sam</span>
           </div>
-          <ul className="hidden lg:flex ml-14 space-x-12"></ul>
+          <ul className="hidden lg:flex ml-14 space-x-12">
+            {navItems.map((items, index) => (
+              <li key={index}>
+                <a href={items.href}>{items.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex items-center justify-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign in
+            </a>
+            <a
+              href="#"
+              className="py-2 px-3 border rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+            >
+              Create an Account
+            </a>
+          </div>
         </div>
       </div>
     </nav>
