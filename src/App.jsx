5daad13d1d6e71ec0c7b7e523b67ab3d290ca662
samstickkz <samstickkz@gmail.com>
2024-07-12 12:09:32 +0100

    features

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Feature from "./components/feature";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 
@@ -7,6 +8,7 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
+        <Feature />
       </div>
     </>
   );
